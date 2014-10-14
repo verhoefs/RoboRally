e03c3cbdc7b262d76b6246a57737e42ec062a595
guidotheelen <guidotheelen@MacBook-Pro-van-Guido-Theelen.local>
2014-10-14 12:29:44 +0200

    inlog/menu
    Eerste opzet

---
+++ roborally/roborally/MainActivity.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+using Android.Graphics;
+
+namespace roborally
+{
+	[Activity (Label = "roborally", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			// Get our button from the layout resource,
+			// and attach an event to it
+			ImageView img1 = FindViewById<ImageView> (Resource.Id.imageView1);
+			ImageView img2 = FindViewById<ImageView> (Resource.Id.imageView2);
+			ImageButton imgbutton = FindViewById<ImageButton> (Resource.Id.imageButton1);
+			Button inlogbutton = FindViewById<Button> (Resource.Id.button1);
+			Button forgotpass = FindViewById<Button> (Resource.Id.button2);
+			Button createaccount = FindViewById<Button> (Resource.Id.button3);
+			EditText emailtext = FindViewById<EditText> (Resource.Id.editText1);
+
+			// Link to pictures
+			img1.SetImageResource (Resource.Drawable.background);
+			img2.SetImageResource (Resource.Drawable.achtergrond2);
+			imgbutton.SetImageResource (Resource.Drawable.facebook);
+
+			inlogbutton.Click += delegate { 
+
+				StartActivity(typeof(SpelMenu));
+			};
+		}
+	}
+}
+
+
+
---
+++ roborally/roborally/SpelMenu.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace roborally
+{
+	[Activity (Label = "SpelMenu")]			
+	public class SpelMenu : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.Menu);
+
+			ImageView imgview = FindViewById<ImageView> (Resource.Id.imageView1);
+
+			imgview.SetImageResource (Resource.Drawable.robotmenu);
+
+			// Create your application here
+		}
+	}
+}
+
